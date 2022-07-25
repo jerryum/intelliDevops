7e9d17d6fd1302fcda6002d3dc7a41e8617eb87c
Jerry Lee <jerry.lee@nexclipper.io>
2022-07-25 15:00:00 -0700

    adjust db parameters

--- src/database/index.ts
+++ src/database/index.ts
@@ -29,10 +29,10 @@ const sequelize = new Sequelize.Sequelize(database, user, password, {
     min: pool.min,
     max: pool.max,
     acquire: 60000,
-    idle: 5000
+    idle: 10000
   },
   logQueryParameters: config.nodeEnv === 'development',
-  logging: console.log,
+//  logging: console.log,
 //  logging: (query, time) => {
 //    logger.info(time + 'ms' + ' ' + query);
 //  },
