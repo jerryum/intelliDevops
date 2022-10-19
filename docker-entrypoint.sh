e40ece5569fa44787a5f6d473457f91a92427031
jerryum <84423164+jerryum@users.noreply.github.com>
2022-10-19 10:16:23 -0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,9 +3,9 @@
 
 if [[ -f /vault/secrets/ncdb-admin-login ]]; then
     source /vault/secrets/ncdb-admin-login
-    export NC_AH_DB_CONFIG_USER="${NC_CRON_DB_CONFIG_USER}"
-    export NC_AH_DB_CONFIG_PASSWORD="${NC_CRON_DB_CONFIG_PASSWORD}"
+    export NC_AH_DB_CONFIG_USER="${NC_AH_DB_CONFIG_USER}"
+    export NC_AH_DB_CONFIG_PASSWORD="${NC_AH_DB_CONFIG_PASSWORD}"
 fi
 
-npm run dev
+npm run start
 
