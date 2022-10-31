f0231f9947036743c28f7eb0a8e364311e0b36ff
Jerry Lee <jerry.lee@nexclipper.io>
2022-10-31 02:07:55 -0700

    remove commonlabel json

--- src/modules/Alerts/services/alerts.service.ts
+++ src/modules/Alerts/services/alerts.service.ts
@@ -35,8 +35,6 @@ class AlertService {
       const node = labels.node || '';
       const startsAt = alerts[i].startsAt;
       const alertId = uuid.v1();
-      const processedCommonLabels = JSON.parse(JSON.stringify(alerts[i].commonLabels));
-      console.log(processedCommonLabels);
 
       let nodeMetricKey;
 
